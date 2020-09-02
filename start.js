cf3b773faf28cb72c5b4764a8096f03f69f18fc9
amorygalili <amorygalili@gmail.com>
2020-09-02 20:58:15 +0000

    wip

--- start.js
+++ start.js
@@ -2,35 +2,55 @@ const { spawn } = require('child_process');
 const fs = require('fs').promises;
 const { LiveStream } = require('./live-stream');
 
-// delete simulateJava.log
+// delete simulateJava.log (maybe not)
 // kill -9 [simulation PID]
 // fuser -k 8080/tcp
 
-const simulateLog = '/workspace/testFRCOnlineIDE/TestProject/build/stdout/simulateJava.log';
-
 async function getSimulationPid() {
-    const data = await fs.readFile('/workspace/testFRCOnlineIDE/TestProject/build/pids/simulateJava.pid', 'utf8');
-    console.log('pid:', data);
-    // return new Buffer(data);
+    return await fs.readFile('/workspace/testFRCOnlineIDE/TestProject/build/pids/simulateJava.pid', 'utf8');
 }
 
-const ls = spawn('./gradlew', ['simulateJava'], {cwd: './TestProject'});
+async function killSimulationPid() { 
+    return new Promise(async resolve => {
+        const pid = await getSimulationPid();
+        const childProcess = spawn('kill', ['-9', pid]);
+        childProcess.on('exit', function (code) {
+            resolve();
+        }); 
+    });
+}
 
+async function killSimulationPort() { 
+    return new Promise(async resolve => {
+        const pid = await getSimulationPid();
+        const childProcess = spawn('fuser', ['-k', '8080/tcp']);
+        childProcess.on('exit', function (code) {
+            resolve();
+        }); 
+    });
+}
 
-// ls = spawn('java', ['-Xmx1024M', '-Xms1024M', '-jar', './../craftbukkit-1.8.7-R0.1-SNAPSHOT-latest.jar', 'nogui'], {cwd: '/some/path/'});
+async function start() {
+    await killSimulationPid();
+    await killSimulationPort();
 
-ls.stdout.on('data', function (data) {
-  console.log('stdout: ' + data.toString());
-});
+    const childProcess = spawn('./gradlew', ['simulateJava'], {cwd: './TestProject'});
+    childProcess.stdout.on('data', function (data) {
+        console.log('stdout: ' + data.toString());
+    });
 
-ls.stderr.on('data', function (data) {
-  console.log('stderr: ' + data.toString());
-});
+    childProcess.stderr.on('data', function (data) {
+        console.log('stderr: ' + data.toString());
+    });
 
-ls.on('exit', function (code) {
-  console.log('child process exited with code ' + code.toString());
-  getSimulationPid();
-  const liveStream = new LiveStream(simulateLog, text => {
-    console.log('text:', text);
-});
-});
+    const simulateLog = '/workspace/testFRCOnlineIDE/TestProject/build/stdout/simulateJava.log';
+
+    childProcess.on('exit', function (code) {
+        console.log('child process exited with code ' + code.toString());
+        const liveStream = new LiveStream(simulateLog, text => {
+            console.log('text:', text);
+        });
+    });
+}
+
+start();
