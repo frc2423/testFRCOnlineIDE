fc47f6e563259063b1b9d0461399b679e6e235de
amorygalili <amorygalili@gmail.com>
2020-09-02 16:38:02 +0000

    simulation is now started in node script

--- start.js
+++ start.js
@@ -1,1 +1,16 @@
-console.log('woo');
+const { spawn } = require('child_process');
+const ls = spawn('./gradlew', ['simulateJava'], {cwd: './TestProject'});
+
+// ls = spawn('java', ['-Xmx1024M', '-Xms1024M', '-jar', './../craftbukkit-1.8.7-R0.1-SNAPSHOT-latest.jar', 'nogui'], {cwd: '/some/path/'});
+
+ls.stdout.on('data', function (data) {
+  console.log('stdout: ' + data.toString());
+});
+
+ls.stderr.on('data', function (data) {
+  console.log('stderr: ' + data.toString());
+});
+
+ls.on('exit', function (code) {
+  console.log('child process exited with code ' + code.toString());
+});
